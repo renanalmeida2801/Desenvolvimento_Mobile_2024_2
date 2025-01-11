faadda0bcb31be8e20314630878af24efb6dc1a6
renanalmeida2801 <renanvictoralmeidasilva@gmail.com>
2025-01-10 21:19:53 -0300

    add ativ RoomDatabase

---
+++ Entrega 3/RoomDatabase/app/src/main/java/com/example/roomdatabase/UserRepository.kt
@@ -0,0 +1,14 @@
+package com.example.roomdatabase
+import kotlinx.coroutines.flow.Flow
+
+
+class UserRepository (private val userDao: UserDao){
+    suspend fun insertUser(user: User) = userDao.insertUser(user)
+
+    fun getAllUsers(): Flow<List<User>> = userDao.getAllUsers()
+
+    suspend fun updateUser(user: User) = userDao.updateUser(user)
+
+    suspend fun deleteUser(user: User) = userDao.deleteUser(user)
+
+}
